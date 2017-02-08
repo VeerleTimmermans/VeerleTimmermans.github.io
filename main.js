74e2ce415650fbec2c7b07d859442a4b83bb7cb0
VeerleTimmermans <veerletimmermanss@gmail.com>
2017-02-08 21:47:57 +0100

    updates

--- main.js
+++ main.js
@@ -1,96 +1,62 @@
 function main(){
-  $('.main h1').hide();
+  $('.jumbotron h1').hide();
   $('.aboutme').hide();
-  $('.past').hide();
-  $('.present').hide();
+  $('.cv').hide();
+  $('.research').hide();
   $('.future').hide();
   $('.portfolio').hide();
-  $('.main h1').fadeIn(4000);
-
+  $('.jumbotron h1').fadeIn(4000);
 
 
   $('#aboutme').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
+    $('.cv').hide();
+    $('.research').hide();
     $('.future').hide(); 
-    $('.firstpage').hide(); 
+    $('.jumbotron').hide(); 
     $('.portfolio').hide(); 
     $('.aboutme').fadeIn(400);});
+
   
-  $('#past').on('click', function(){
-    $('.aboutme').hide();
-    $('.present').hide();
-    $('.future').hide();
-    $('.firstpage').hide(); 
-    $('.portfolio').hide();     
-    $('.past').fadeIn(400);});
-
-  $('#present').on('click', function(){
-    $('.past').hide();
-    $('.aboutme').hide();
-    $('.future').hide(); 
-    $('.firstpage').hide(); 
-    $('.portfolio').hide();    
-    $('.present').fadeIn(400);});
-
-
-  $('#future').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
-    $('.aboutme').hide(); 
-    $('.firstpage').hide();
-    $('.portfolio').hide();    
-    $('.future').fadeIn(400);});
-
-  $('#portfolio').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
-    $('.aboutme').hide(); 
-    $('.firstpage').hide();
-    $('.future').hide();    
-    $('.portfolio').fadeIn(400);});
-
   $('#naboutme').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
+    $('.cv').hide();
+    $('.research').hide();
     $('.future').hide(); 
-    $('.firstpage').hide(); 
+    $('.jumbotron').hide(); 
     $('.portfolio').hide(); 
     $('.aboutme').fadeIn(400);});
   
-  $('#npast').on('click', function(){
+  $('#ncv').on('click', function(){
     $('.aboutme').hide();
-    $('.present').hide();
+    $('.research').hide();
     $('.future').hide();
-    $('.firstpage').hide(); 
+    $('.jumbotron').hide(); 
     $('.portfolio').hide();     
-    $('.past').fadeIn(400);});
+    $('.cv').fadeIn(400);});
 
-  $('#npresent').on('click', function(){
-    $('.past').hide();
+  $('#nresearch').on('click', function(){
+    $('.cv').hide();
     $('.aboutme').hide();
     $('.future').hide(); 
-    $('.firstpage').hide(); 
+    $('.jumbotron').hide();  
     $('.portfolio').hide();    
-    $('.present').fadeIn(400);});
+    $('.research').fadeIn(400);});
 
 
   $('#nfuture').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
+    $('.cv').hide();
+    $('.research').hide();
     $('.aboutme').hide(); 
-    $('.firstpage').hide();
+    $('.jumbotron').hide(); 
     $('.portfolio').hide();    
     $('.future').fadeIn(400);});
 
   $('#nportfolio').on('click', function(){
-    $('.past').hide();
-    $('.present').hide();
+    $('.cv').hide();
+    $('.research').hide();
     $('.aboutme').hide(); 
-    $('.firstpage').hide();
+    $('.jumbotron').hide(); 
     $('.future').hide();    
     $('.portfolio').fadeIn(400);});
-
 }
 
 $(document).ready(main);
